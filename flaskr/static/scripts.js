02b97e48f973bb360c4e3b19e4c8c34dc376ef38
bdoonan <80231384+bdoonan@users.noreply.github.com>
2024-08-15 21:57:43 -0400

    Add files via upload

---
+++ flaskr/static/scripts.js
@@ -0,0 +1,39 @@
+function toggleText(str) {
+    var text = document.getElementById(str);
+    if (text.style.display === "none") {
+      text.style.display = "inline";
+      bluehint.style.display = "none";
+      redhint.style.display = "inline";
+    } 
+}
+function toggleText2(str){
+    var text = document.getElementById(str);
+    if (text.style.display === "none") {
+      text.style.display = "inline";
+      redhint.style.display = "none";
+    } 
+}
+function revealText(){
+    for (let i = 1; i < 11; i++) {
+        let num = i.toString();
+        var text = document.getElementById(num);
+        text.style.display = "inline";
+    }
+    
+}
+function higherLower(str){
+  if (str=="higher"){
+    document.getElementById("higher").style.display == "inline";
+  }
+  else if ("int==lower"){
+    document.getElementById("lower").style.display == "inline";
+  }
+  }
+function checkcomplete(str){
+  const done = localStorage.getItem('done');
+  if (done != str){
+    localStorage.removeItem('done');
+    localStorage.removeItem('completed');
+    localStorage.setItem('done',str)
+  }
+}
